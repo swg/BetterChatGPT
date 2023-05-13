511ae65c6e32b800aeecb4c501a577203befad55
swg <git@swg.me>
2023-05-12 21:40:56 -0400

    Fix opening upward/downward in useDropDown

--- src/hooks/useDropDown.tsx
+++ src/hooks/useDropDown.tsx
@@ -1,9 +1,9 @@
-import React, { ReactNode, useState, useEffect } from 'react';
+import React, { ReactNode, useState, useEffect, useRef } from 'react';
 import useHideOnOutsideClick from './useHideOnOutsideClick';
 
 import DownChevronArrow from '@icon/DownChevronArrow';
 
-const useSelector = ({
+const useDropDown = ({
   selected,
   selections,
   onClick,
@@ -14,17 +14,31 @@ const useSelector = ({
   onClick: React.MouseEventHandler<HTMLElement>;
   icon?: React.ReactElement;
 }) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
   const [dropDown, setDropDown, dropDownRef] = useHideOnOutsideClick();
+
   const [openDirection, setOpenDirection] = useState('down');
-  
+  const [checkPosition, setCheckPosition] = useState(false);
+
   const checkOpenDirection = () => {
-    if (!dropDownRef.current) return;
-    const rect = dropDownRef.current.getBoundingClientRect();
-    setOpenDirection(rect.bottom > window.innerHeight ? 'up' : 'down');
+    if (!buttonRef.current || !dropDownRef.current) return;
+    const buttonRect = buttonRef.current.getBoundingClientRect();
+    const dropDownRect = dropDownRef.current.getBoundingClientRect();
+    setOpenDirection(
+      buttonRect.bottom + dropDownRect.height > window.innerHeight
+        ? 'up'
+        : 'down'
+    );
   };
 
   useEffect(() => {
-    checkOpenDirection();
+    if (checkPosition) {
+      checkOpenDirection();
+      setCheckPosition(false);
+    }
+  }, [checkPosition]);
+
+  useEffect(() => {
     window.addEventListener('resize', checkOpenDirection);
     return () => {
       window.removeEventListener('resize', checkOpenDirection);
@@ -34,19 +48,23 @@ const useSelector = ({
   return (
     <div className='prose dark:prose-invert relative'>
       <button
+        ref={buttonRef}
         className='btn btn-neutral btn-small flex gap-1'
         type='button'
-        onClick={() => setDropDown((prev) => !prev)}
+        onClick={() => {
+          setDropDown((prev) => !prev);
+          setCheckPosition(true);
+        }}
       >
         {selected}
         {icon}
       </button>
       <div
-        ref={dropDownRef}
+        ref={dropDownRef as React.RefObject<HTMLDivElement>}
         id='dropdown'
-        className={`${
-          dropDown ? '' : 'hidden'
-        } absolute top-100 bottom-100 z-10 bg-white rounded-lg shadow-xl border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800 opacity-90`}
+        className={`${dropDown ? '' : 'hidden'} absolute z-10 bg-white text-gray-800 group opacity-90 border-b border-black/10 ${
+          openDirection === 'down' ? 'top-full' : 'bottom-full'
+        } rounded-lg shadow-xl dark:border-gray-900/50 dark:text-gray-100 dark:bg-gray-800`}
       >
         <ul
           className='text-sm text-gray-700 dark:text-gray-200 p-0 m-0'
@@ -70,4 +88,4 @@ const useSelector = ({
   );
 };
 
-export default useSelector;
+export default useDropDown;
